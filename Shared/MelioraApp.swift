f1a57ff1dc89b182698b41038ee95771ade6bbe6
Yura Filatov <yura.fila@gmail.com>
2022-04-18 20:29:04 -0400

    Initial Commit

---
+++ Shared/MelioraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MelioraApp.swift
+//  Shared
+//
+//  Created by Yura on 4/18/22.
+//
+
+import SwiftUI
+
+@main
+struct MelioraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
